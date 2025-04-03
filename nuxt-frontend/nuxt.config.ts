e8be57e44231813fea0c1912afb26b64cb828431
YF Leong <yfbochap@gmail.com>
2025-04-03 09:49:41 +0800

    update nuxt.config.ts

--- nuxt-frontend/nuxt.config.ts
+++ nuxt-frontend/nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
             login: '/login',
             callback: '/confirm',
             include: ['/grading'],
-            exclude: ['/', '/signup', '/marketplace', '/collection', '/listing/**',],
+            exclude: ['/', '/signup', '/marketplace', '/collection', '/listing/**', '/sell'],
             saveRedirectToCookie: true,
         }
     }
