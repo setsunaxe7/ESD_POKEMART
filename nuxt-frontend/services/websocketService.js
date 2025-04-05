392381acfbe75ae30f772b65c19150fd5eea9abf
YF Leong <yfbochap@gmail.com>
2025-04-05 12:39:31 +0800

    merge

--- nuxt-frontend/services/websocketService.js
+++ nuxt-frontend/services/websocketService.js
@@ -48,17 +48,40 @@ class WebSocketService {
     }
 
     subscribeToQueue(queue, onMessageReceived) {
+        if (!this.client) {
+            console.error("WebSocket client is not connected!");
+            return;
+        }
+
         this.client.subscribe(`/queue/${queue}`, (message) => {
             if (message.body) {
-                onMessageReceived(JSON.parse(message.body));
+                const parsedMessage = JSON.parse(message.body);
+                console.log(`Received message from queue '${queue}':`, parsedMessage);
+                onMessageReceived(parsedMessage);
             }
         });
-    }   
+    }
+
+    // subscribeToExchange(exchange, bindingKey, onMessageReceived) {
+    //     this.client.subscribe(`/exchange/${exchange}/${bindingKey}`, (message) => {
+    //         if (message.body) {
+    //             onMessageReceived(JSON.parse(message.body));
+    //         }
+    //     });
+    // }
 
     subscribeToExchange(exchange, bindingKey, onMessageReceived) {
+        if (!this.client) {
+            console.error("WebSocket client is not connected!");
+            return;
+        }
+    
+        // Subscribe to the exchange with the specified binding key
         this.client.subscribe(`/exchange/${exchange}/${bindingKey}`, (message) => {
             if (message.body) {
-                onMessageReceived(JSON.parse(message.body));
+                const parsedMessage = JSON.parse(message.body);
+                console.log(`Received message from exchange '${exchange}' with binding key '${bindingKey}':`, parsedMessage);
+                onMessageReceived(parsedMessage);
             }
         });
     }
@@ -81,9 +104,9 @@ class WebSocketService {
     //     }
     // }
 
-    subscribeToBids(onMessageReceived) {
-        this.subscribeToExchange("bidding_exchange", "", onMessageReceived);
-    }
+    // subscribeToBids(onMessageReceived) {
+    //     this.subscribeToExchange("bidding_exchange", "", onMessageReceived);
+    // }
 
     disconnect() {
         if (this.client) {
